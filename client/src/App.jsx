9ba161eb20b46b5caf521245197a0ba0c2609584
pratikwayase <pratikwayase@gmail.com>
2025-02-20 15:13:56 +0530

    intial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-orange-800'>
+      hellos
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/student/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-blue-700'>
+      hello
+    </div>
+  )
+}
+
+export default Navbar
