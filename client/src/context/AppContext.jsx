b8df2510a6bea971838a38043c90677a648ba1d1
pratikwayase <pratikwayase@gmail.com>
2025-02-23 11:18:20 +0530

    /

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,8 +4,12 @@ export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
 
+    const value = {
+
+    }
+
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={value}>
             {props.children}
         </AppContext.Provider>
     )
