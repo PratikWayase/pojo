d20c9b253f52a1e976ebdf9e94118f07b4ea4e42
pratikwayase <pratikwayase@gmail.com>
2025-02-27 22:54:38 +0530

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Student/Home'
+import CourseList from './pages/Student/CourseList'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/CourseList ' element={CourseList} />
+        <Route path='/CourseList ' element= {CourseList } />
       </Routes>
       
     </div>
