bbf6393b2dff496be91bfba01112a773116582be
pratikwayase <pratikwayase@gmail.com>
2025-02-27 22:56:30 +0530

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Student/Home'
 import CourseList from './pages/Student/CourseList'
+import CourseDetails from './pages/Student/CourseDetails'
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/CourseList ' element={CourseList} />
-        <Route path='/CourseList ' element= {CourseList } />
+        <Route path='/Course/:id ' element= {CourseDetails} />
       </Routes>
       
     </div>
