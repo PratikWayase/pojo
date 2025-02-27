65299a654a6f81ee67f344866e4ba8b15ea35365
pratikwayase <pratikwayase@gmail.com>
2025-02-27 23:01:37 +0530

    /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Student/Home'
 import CourseList from './pages/Student/CourseList'
 import CourseDetails from './pages/Student/CourseDetails'
 import MyEnrollment from './pages/Student/MyEnrollment'
+import Player from './pages/Student/Player'
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
         <Route path='/CourseList ' element={<CourseList />} />
         <Route path='/CourseList/:input ' element={<CourseList/>} />
         <Route path='/Course/:id ' element={<CourseDetails/>} />
-        <Route path='/MyEnrollment' element={<MyEnrollment/>} />
+        <Route path='/MyEnrollment' element={<MyEnrollment />} />
+        <Route path='/player/:courseId' element={<Player/>} />
     
       </Routes>
       
